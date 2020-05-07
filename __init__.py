c047d770ac334d6af11402d2a3065a63b2cd5313
stratus-ss <steve.ovens@x86innovations.com>
2020-05-07 19:07:45 -0400

    initial MVP commit of recipe helper converts butter measurements to weight converts standard baking units between metric and imperial

---
+++ __init__.py
@@ -0,0 +1,256 @@
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.skills.context import adds_context, removes_context
+from mycroft import intent_file_handler
+from mycroft.util.log import getLogger
+from adapt.intent import IntentBuilder
+
+
+class RecipeHelperSkill(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+        self.cups_ml_constant_number = 0.0042268
+        self.cups_litres_constant_number = 4.2268
+        self.pints_ml_constant_number = 0.0021134
+        self.pints_litres_constant_number = 2.1134
+        self.fl_oz_ml_constant_number = 0.033814
+        self.fl_oz_litres_constant_number = 33.814
+        self.grams_oz_constant_number = 0.035274
+        self.grams_lbs_constant_number = 0.0022046
+        self.convert_from_this_unit = ''
+        self.convert_to_this_unit = ''
+        self.number_of_new_units = ''
+
+    @staticmethod
+    def convert_ml_to_litres(number_of_ml):
+        number_of_litres = round((number_of_ml / 1000), 1)
+        return number_of_litres
+
+    def convert_cups_to_ml(self, number_of_cups):
+        number_of_ml = round((number_of_cups / self.cups_ml_constant_number), 1)
+        return number_of_ml
+
+    def convert_cups_to_litres(self, number_of_cups):
+        number_of_litres = round((number_of_cups / self.cups_litres_constant_number), 1)
+        return number_of_litres
+
+    def convert_ml_to_cups(self, number_of_ml):
+        number_of_cups = round((number_of_ml * self.cups_ml_constant_number), 1)
+        return number_of_cups
+
+    def convert_litres_to_cups(self, number_of_litres):
+        number_of_cups = round((number_of_litres * self.cups_litres_constant_number), 1)
+        return number_of_cups
+
+    def convert_pints_to_ml(self, number_of_pints):
+        number_of_ml = round((number_of_pints / self.pints_ml_constant_number), 1)
+        return number_of_ml
+
+    def convert_pints_to_litres(self, number_of_pints):
+        number_of_litres = round((number_of_pints / self.pints_litres_constant_number), 1)
+        return number_of_litres
+
+    def convert_ml_to_pints(self, number_of_ml):
+        number_of_pints = round((number_of_ml * self.pints_ml_constant_number), 1)
+        return number_of_pints
+
+    def convert_litres_to_pints(self, number_of_litres):
+        number_of_pints = round((number_of_litres * self.pints_litres_constant_number), 1)
+        return number_of_pints
+
+    def convert_fl_oz_to_ml(self, number_of_fl_oz):
+        number_of_ml = round((number_of_fl_oz / self.fl_oz_ml_constant_number), 1)
+        return number_of_ml
+
+    def convert_fl_oz_to_litres(self, number_of_fl_oz):
+        number_of_litres = round((number_of_fl_oz / self.fl_oz_litres_constant_number), 1)
+        return number_of_litres
+
+    def convert_ml_to_fl_oz(self, number_of_ml):
+        number_of_fl_oz = round((number_of_ml * self.fl_oz_ml_constant_number), 1)
+        print(number_of_fl_oz)
+        return number_of_fl_oz
+
+    def convert_litres_to_fl_oz(self, number_of_litres):
+        number_of_fl_oz = round((number_of_litres * self.fl_oz_litres_constant_number), 1)
+        return number_of_fl_oz
+
+    def convert_oz_to_grams(self, number_of_oz):
+        number_of_grams = round((number_of_oz / self.grams_oz_constant_number), 1)
+        return number_of_grams
+
+    @staticmethod
+    def convert_oz_to_lbs(number_of_oz):
+        number_of_lbs = round((number_of_oz / 16), 1)
+        return number_of_lbs
+
+    def convert_lbs_to_grams(self, number_of_lbs):
+        number_of_grams = round((number_of_lbs / self.grams_lbs_constant_number), 1)
+        return number_of_grams
+
+    @staticmethod
+    def convert_lbs_to_oz(number_of_lbs):
+        number_of_ounces = round((number_of_lbs * 16), 1)
+        return number_of_ounces
+
+    def convert_grams_to_oz(self, number_of_grams):
+        number_of_oz = round((number_of_grams * self.grams_oz_constant_number), 1)
+        return number_of_oz
+
+    def convert_grams_to_lbs(self, number_of_grams):
+        number_of_lbs = round((number_of_grams * self.grams_lbs_constant_number), 1)
+        return number_of_lbs
+
+    @staticmethod
+    def convert_cel_to_fahr(celsius):
+        fahrenheit = (celsius * 1.8) + 32
+        return fahrenheit
+
+    @staticmethod
+    def convert_fahr_to_cel(fahrenheit):
+        celsius = (fahrenheit - 32) / 1.8
+        return celsius
+
+    @staticmethod
+    def butter_weight_from_tablespoon(unit='grams', number_of_tablespoons=1):
+        unit_of_measurement = unit.lower()
+        if unit_of_measurement == "grams":
+            number_of_grams = number_of_tablespoons * 14.19
+            return number_of_grams
+        if unit_of_measurement == "ounces":
+            number_of_oz = number_of_tablespoons * 0.50
+            return number_of_oz
+        if unit_of_measurement == "lbs" or unit_of_measurement == "pounds":
+            number_of_lbs = number_of_tablespoons * 0.03
+            return number_of_lbs
+
+    @staticmethod
+    def butter_weight_from_teaspoon(unit='grams', number_of_teaspoons=1):
+        unit_of_measurement = unit.lower()
+        if unit_of_measurement == "grams":
+            number_of_grams = number_of_teaspoons * 4.73
+            return number_of_grams
+        if unit_of_measurement == "ounces":
+            number_of_oz = number_of_teaspoons * 0.17
+            return number_of_oz
+        if unit_of_measurement == "lbs" or unit_of_measurement == "pounds":
+            number_of_lbs = number_of_teaspoons * 0.01
+            return number_of_lbs
+
+    @staticmethod
+    def butter_weight_from_cups(unit='grams', number_of_cups=1):
+        unit_of_measurement = unit.lower()
+        if unit_of_measurement == "grams":
+            number_of_grams = number_of_cups * 227
+            return number_of_grams
+        if unit_of_measurement == "ounces":
+            number_of_oz = number_of_cups * 8.01
+            return number_of_oz
+        if unit_of_measurement == "lbs" or unit_of_measurement == "pounds":
+            number_of_lbs = number_of_cups * 0.50
+            return number_of_lbs
+
+    def get_converted_units(self):
+        return int(self.convert_from_this_unit.split()[0])
+
+    def speak_conversion_weight(self):
+        sentence = "%s weighs %s %s" % (self.convert_from_this_unit, self.number_of_new_units,
+                                        self.convert_to_this_unit)
+        self.speak(sentence)
+
+    def speak_conversion_generic(self):
+        sentence = "%s is %s %s" % (self.convert_from_this_unit, self.number_of_new_units,
+                                        self.convert_to_this_unit)
+        self.speak(sentence)
+
+    @intent_file_handler("convert.butter.intent")
+    def convert_butter(self, message):
+        self.convert_from_this_unit = message.data.get('oldunit')
+        self.convert_to_this_unit = message.data.get('newunit')
+        if "butter" in message.data['utterance']:
+            if "teaspoon" in self.convert_from_this_unit:
+                self.number_of_new_units = self.butter_weight_from_teaspoon(unit=self.convert_to_this_unit,
+                                                                            number_of_teaspoons=self.get_converted_units())
+            elif "tablespoon" in message.data['utterance']:
+                self.number_of_new_units = self.butter_weight_from_tablespoon(unit=self.convert_to_this_unit,
+                                                                              number_of_tablespoons=self.get_converted_units())
+            elif "cups" in message.data['utterance']:
+                self.number_of_new_units = self.butter_weight_from_cups(unit=self.convert_to_this_unit,
+                                                                        number_of_cups=self.get_converted_units())
+            else:
+                pass
+            self.speak_conversion_weight()
+
+    @intent_file_handler("convert.units.intent")
+    def convert_units(self, message):
+        self.convert_from_this_unit = message.data.get('oldunit')
+        self.convert_to_this_unit = message.data.get('newunit')
+        number_of_units_to_convert = self.get_converted_units()
+        if "milliliter" in self.convert_from_this_unit or "ml" in self.convert_from_this_unit:
+            if "liter" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_ml_to_litres(number_of_units_to_convert)
+            elif "cup" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_ml_to_cups(number_of_units_to_convert)
+            elif "pint" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_ml_to_pints(number_of_units_to_convert)
+            elif "fluid ounce" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_ml_to_fl_oz(number_of_units_to_convert)
+            else:
+                exit()
+        elif "gram" in self.convert_from_this_unit:
+            if "ounce" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_grams_to_oz(number_of_units_to_convert)
+            elif "lbs" in self.convert_to_this_unit or "pounds" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_grams_to_lbs(number_of_units_to_convert)
+            else:
+                exit()
+        elif "liter" in self.convert_from_this_unit:
+            if "cup" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_litres_to_cups(number_of_units_to_convert)
+            elif "pints" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_litres_to_pints(number_of_units_to_convert)
+            elif "fluid ounce" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_litres_to_fl_oz(number_of_units_to_convert)
+            else:
+                exit()
+        elif "pounds" in self.convert_from_this_unit or "lbs" in self.convert_from_this_unit:
+            if "ounce" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_lbs_to_oz(number_of_units_to_convert)
+            elif "gram" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_lbs_to_grams(number_of_units_to_convert)
+            else:
+                exit()
+        elif "cup" in self.convert_from_this_unit:
+            if "milliliter" in self.convert_to_this_unit:
+                self.number_of_new_units = self.convert_cups_to_ml(number_of_units_to_convert)
+            elif self.convert_to_this_unit.startswith("liter"):
+                self.number_of_new_units = self.convert_cups_to_litres(number_of_units_to_convert)
+            else:
+                exit()
+        elif "pint" in self.convert_from_this_unit:
+            if self.convert_to_this_unit.startswith("milliliter"):
+                self.number_of_new_units = self.convert_pints_to_ml(number_of_units_to_convert)
+            elif self.convert_to_this_unit.startswith("liter"):
+                self.number_of_new_units = self.convert_pints_to_litres(number_of_units_to_convert)
+            else:
+                exit()
+        elif "fluid" in self.convert_from_this_unit:
+            if self.convert_to_this_unit.startswith("milliliter"):
+                self.number_of_new_units = self.convert_fl_oz_to_ml(number_of_units_to_convert)
+            elif self.convert_to_this_unit.startswith("liter"):
+                self.number_of_new_units = self.convert_fl_oz_to_litres(number_of_units_to_convert)
+            else:
+                exit()
+        elif "ounce" in self.convert_from_this_unit:
+            if self.convert_to_this_unit.startswith("pound") or self.convert_to_this_unit.startswith("lbs"):
+                self.number_of_new_units = self.convert_oz_to_lbs(number_of_units_to_convert)
+            elif self.convert_to_this_unit.startswith("gram"):
+                self.number_of_new_units = self.convert_oz_to_grams(number_of_units_to_convert)
+            else:
+                exit()
+        else:
+            pass
+        self.speak_conversion_generic()
+
+
+def create_skill():
+    return RecipeHelperSkill()
---
+++ recipes_helper.py
@@ -0,0 +1,125 @@
+cups_ml_constant_number = 0.0042268
+cups_litres_constant_number = 4.2268
+pints_ml_constant_number = 0.0021134
+pints_litres_constant_number = 2.1134
+fl_oz_ml_constant_number = 0.033814
+fl_oz_litres_constant_number = 33.814
+grams_oz_constant_number = 0.035274
+grams_lbs_constant_number = 0.0022046
+
+def convert_ml_to_litres(number_of_ml):
+    number_of_litres = number_of_ml * 1000
+    return number_of_litres
+
+def convert_cups_to_ml(number_of_cups):
+    number_of_ml = number_of_cups / cups_ml_constant_number
+    return number_of_ml
+
+def convert_cups_to_litres(number_of_cups):
+    number_of_litres = number_of_cups / cups_litres_constant_number
+    return number_of_litres
+
+def convert_ml_to_cups(number_of_ml):
+    number_of_cups = number_of_ml * cups_ml_constant_number
+    return number_of_cups
+
+def convert_litres_to_cups(number_of_litres):
+    number_of_cups = number_of_litres * cups_litres_constant_number
+    return number_of_cups
+
+def convert_pints_to_ml(number_of_pints):
+    number_of_ml = number_of_pints / pints_ml_constant_number
+    return number_of_ml
+
+def convert_pints_to_litres(number_of_pints):
+    number_of_litres = number_of_pints / pints_litres_constant_number
+    return number_of_litres
+
+def convert_ml_to_pints(number_of_ml):
+    number_of_pints = number_of_ml * pints_ml_constant_number
+    return number_of_pints
+
+def convert_litres_to_pints(number_of_litres):
+    number_of_pints = number_of_litres * pints_litres_constant_number
+    return number_of_pints
+
+def convert_fl_oz_to_ml(number_of_fl_oz):
+    number_of_ml = number_of_fl_oz / fl_oz_ml_constant_number
+    return number_of_ml
+
+def convert_fl_oz_to_litres(number_of_fl_oz):
+    number_of_litres = number_of_fl_oz / fl_oz_litres_constant_number
+    return number_of_litres
+
+def convert_ml_to_fl_oz(number_of_ml):
+    number_of_fl_oz = number_of_ml * fl_oz_ml_constant_number
+    return number_of_fl_oz
+
+def convert_litres_to_fl_oz(number_of_litres):
+    number_of_fl_oz = number_of_litres * fl_oz_litres_constant_number
+    return number_of_fl_oz
+
+def convert_oz_to_grams(number_of_oz):
+    number_of_grams = number_of_oz / grams_oz_constant_number
+    return number_of_grams
+
+def convert_lbs_to_grams(number_of_lbs):
+    number_of_grams = number_of_lbs / grams_lbs_constant_number
+    return number_of_grams
+
+def convert_grams_to_oz(number_of_grams):
+    number_of_oz = number_of_grams * grams_oz_constant_number
+    return number_of_oz
+
+def convert_grams_to_lbs(number_of_grams):
+    number_of_lbs = number_of_grams * grams_lbs_constant_number
+    return number_of_lbs
+
+def convert_cel_to_fahr(celcius):
+    fahrenheit = (celcius * 1.8) + 32
+    return (fahrenheit)
+
+def convert_fahr_to_cel(fahrenheit):
+    celcius = (fahrenheit - 32) / 1.8
+    return celcius
+
+
+def butter_from_tablespoon_conversion(unit='grams', number_of_tablespoons):
+    unit_of_measurement = units.lower()
+    if unit_of_measurement == "grams":
+        number_of_grams = number_of_tablespoons * 14.19
+        return number_of_grams
+    if unit_of_measurement == "ounces":
+        number_of_oz = number_of_tablespoons * 0.50
+        return number_of_oz
+    if unit_of_measurement == "lbs" of unit_of_measurement == "pounds":
+        number_of_lbs = number_of_tablespoons * 0.03
+        return number_of_lbs
+
+def butter_from_teaspoon_conversion(unit='grams', number_of_teaspoons):
+    unit_of_measurement = units.lower()
+    if unit_of_measurement == "grams":
+        number_of_grams = number_of_teaspoons * 4.73
+        return number_of_grams
+    if unit_of_measurement == "ounces":
+        number_of_oz = number_of_teaspoons * 0.17
+        return number_of_oz
+    if unit_of_measurement == "lbs" of unit_of_measurement == "pounds":
+        number_of_lbs = number_of_teaspoons * 0.01
+        return number_of_lbs
+
+def butter_from_cups_conversion(unit='grams', number_of_cups):
+    unit_of_measurement = units.lower()
+    if unit_of_measurement == "grams":
+        number_of_grams = number_of_cups * 227
+        return number_of_grams
+    if unit_of_measurement == "ounces":
+        number_of_oz = number_of_cups * 8.01
+        return number_of_oz
+    if unit_of_measurement == "lbs" of unit_of_measurement == "pounds":
+        number_of_lbs = number_of_cups * 0.50
+        return number_of_lbs
+
+
+
+
